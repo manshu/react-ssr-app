1fc5b5c61df6fd1be7c2b3a7ba331f05ae66629d
Himanshu Batra <himanshu@batra.dev>
2019-12-26 15:27:26 -0500

    WiP

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'react-ssr',
+            script: './start.js',
+            env: {
+                HOST: 'localhost',
+                PORT: 3000
+            }
+        }
+    ],
+}
